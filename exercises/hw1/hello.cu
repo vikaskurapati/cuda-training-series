3ff0cd20934bbf5c23175bf60802e27f801ce2f8
Vikas Kurapati <vikas.kurapati@tum.de>
2025-06-10 11:21:57 +0200

    Introductory parts, hello world, vector addition, and basic matrix multiplication done

--- exercises/hw1/hello.cu
+++ exercises/hw1/hello.cu
@@ -2,12 +2,22 @@
 
 __global__ void hello(){
 
-  printf("Hello from block: %u, thread: %u\n", FIXME);
+  printf("Hello from block: %u, thread: %u\n", blockIdx.x, threadIdx.x);
+}
+
+//Macro for checking cuda errors following a cuda launch or api call
+#define cudaCheckError() {                                          \
+ cudaError_t e=cudaGetLastError();                                 \
+ if(e!=cudaSuccess) {                                              \
+   printf("Cuda failure %s:%d: '%s'\n",__FILE__,__LINE__,cudaGetErrorString(e));           \
+   exit(0); \
+ }                                                                 \
 }
 
 int main(){
 
-  hello<<<FIXME>>>();
+  hello<<<3,3>>>();
+  cudaCheckError();
   cudaDeviceSynchronize();
 }
 
